236b1836ed084376659a60113e1bd1208f8d4e28
Yoshihiro Tanaka <contact@cordea.jp>
2022-05-15 12:54:14 +0900

    add an app bar

--- app/src/main/java/jp/cordea/d7362089/ui/home/Home.kt
+++ app/src/main/java/jp/cordea/d7362089/ui/home/Home.kt
@@ -4,14 +4,13 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.ColorPainter
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -20,7 +19,17 @@ import jp.cordea.d7362089.ui.theme.D7362089Theme
 @Composable
 @ExperimentalMaterial3Api
 fun Home() {
-    Scaffold {
+    val scrollBehavior = remember { TopAppBarDefaults.pinnedScrollBehavior() }
+    Scaffold(
+        modifier = Modifier
+            .nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            SmallTopAppBar(
+                title = { Text("Home") },
+                scrollBehavior = scrollBehavior
+            )
+        }
+    ) {
         LazyColumn(contentPadding = it) {
             item { SectionLabel() }
             item { Pickup() }
