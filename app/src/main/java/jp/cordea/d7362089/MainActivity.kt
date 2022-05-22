a08cfe98d55d2d9e2f675f6ed501a4cff6eeb808
Yoshihiro Tanaka <contact@cordea.jp>
2022-05-22 09:32:38 +0900

    resolve error

--- app/src/main/java/jp/cordea/d7362089/MainActivity.kt
+++ app/src/main/java/jp/cordea/d7362089/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.hilt.navigation.compose.hiltViewModel
+import coil.annotation.ExperimentalCoilApi
 import dagger.hilt.android.AndroidEntryPoint
 import jp.cordea.d7362089.ui.home.Home
 import jp.cordea.d7362089.ui.theme.D7362089Theme
