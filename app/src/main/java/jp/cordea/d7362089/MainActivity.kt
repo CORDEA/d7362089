bfc8d70d67dd3687cf5701bf5f55c4a689550e5e
Yoshihiro Tanaka <contact@cordea.jp>
2022-05-28 10:17:55 +0900

    introduce navigation

--- app/src/main/java/jp/cordea/d7362089/MainActivity.kt
+++ app/src/main/java/jp/cordea/d7362089/MainActivity.kt
@@ -4,10 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.hilt.navigation.compose.hiltViewModel
 import coil.annotation.ExperimentalCoilApi
 import dagger.hilt.android.AndroidEntryPoint
-import jp.cordea.d7362089.ui.home.Home
+import jp.cordea.d7362089.ui.NavGraph
 import jp.cordea.d7362089.ui.theme.D7362089Theme
 
 @AndroidEntryPoint
@@ -18,7 +17,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             D7362089Theme {
-                Home(hiltViewModel())
+                NavGraph()
             }
         }
     }
---
+++ app/src/main/java/jp/cordea/d7362089/ui/NavGraph.kt
@@ -0,0 +1,33 @@
+package jp.cordea.d7362089.ui
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import coil.annotation.ExperimentalCoilApi
+import jp.cordea.d7362089.ui.details.Details
+import jp.cordea.d7362089.ui.home.Home
+
+object Destination {
+    const val HOME = "home"
+    const val DETAILS = "details"
+}
+
+@Composable
+@ExperimentalCoilApi
+@ExperimentalMaterial3Api
+fun NavGraph(
+    navController: NavHostController = rememberNavController()
+) {
+    NavHost(navController = navController, startDestination = Destination.HOME) {
+        composable(route = Destination.HOME) {
+            Home(hiltViewModel())
+        }
+        composable(route = Destination.DETAILS) {
+            Details(hiltViewModel())
+        }
+    }
+}
