69d439c6a62a00eeefa5be28f5146213c3b0f138
Yoshihiro Tanaka <contact@cordea.jp>
2022-05-28 10:12:34 +0900

    add new page

---
+++ app/src/main/java/jp/cordea/d7362089/ui/details/Details.kt
@@ -0,0 +1,7 @@
+package jp.cordea.d7362089.ui.details
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun Details(viewModel: DetailsViewModel) {
+}
---
+++ app/src/main/java/jp/cordea/d7362089/ui/details/DetailsViewModel.kt
@@ -0,0 +1,10 @@
+package jp.cordea.d7362089.ui.details
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class DetailsViewModel @Inject constructor() : ViewModel() {
+
+}
