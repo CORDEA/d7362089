2a54c1ec0c8ed6e3fd3b3a2eded885580080c32a
Yoshihiro Tanaka <contact@cordea.jp>
2022-06-11 11:32:30 +0900

    bind with viewmodel

--- app/src/main/java/jp/cordea/d7362089/ui/details/Details.kt
+++ app/src/main/java/jp/cordea/d7362089/ui/details/Details.kt
@@ -4,24 +4,29 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.painter.ColorPainter
 import androidx.compose.ui.input.nestedscroll.NestedScrollConnection
 import androidx.compose.ui.input.nestedscroll.NestedScrollSource
 import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.dp
+import coil.annotation.ExperimentalCoilApi
+import coil.compose.rememberImagePainter
 
 @Composable
-@OptIn(ExperimentalMaterial3Api::class)
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalCoilApi::class)
 fun Details(viewModel: DetailsViewModel) {
     val appBarMaxHeight = with(LocalDensity.current) { 250.dp.toPx() }
     val appBarMinHeight = with(LocalDensity.current) { 64.dp.toPx() }
@@ -35,6 +40,9 @@ fun Details(viewModel: DetailsViewModel) {
             }
         }
     }
+    val thumbnail by viewModel.thumbnail.observeAsState()
+    val description by viewModel.description.observeAsState()
+    val userName by viewModel.userName.observeAsState()
     Scaffold(
         modifier = Modifier.nestedScroll(scrollConnection),
         topBar = {
@@ -43,9 +51,9 @@ fun Details(viewModel: DetailsViewModel) {
                     .height(with(LocalDensity.current) { appBarHeight.value.toDp() })
             ) {
                 Image(
-                    // TODO
-                    painter = ColorPainter(Color.Cyan),
-                    contentDescription = "",
+                    painter = rememberImagePainter(data = thumbnail),
+                    contentDescription = description,
+                    contentScale = ContentScale.Crop,
                     modifier = Modifier.fillMaxSize()
                 )
                 SmallTopAppBar(
@@ -53,14 +61,18 @@ fun Details(viewModel: DetailsViewModel) {
                     colors = TopAppBarDefaults.smallTopAppBarColors(
                         containerColor = Color.Transparent
                     ),
-                    title = { Text("") },
+                    title = { Text("By $userName") },
                 )
             }
         }
     ) {
         LazyColumn(
-            contentPadding = it
+            contentPadding = it,
+            modifier = Modifier.padding(16.dp),
         ) {
+            item {
+                Text(description.orEmpty())
+            }
         }
     }
 }
