169cf48d68eb4404932c9694bd4fa30203813d11
Yoshihiro Tanaka <contact@cordea.jp>
2022-06-12 14:00:57 +0900

    resolve error

--- app/src/main/java/jp/cordea/d7362089/ui/home/Home.kt
+++ app/src/main/java/jp/cordea/d7362089/ui/home/Home.kt
@@ -8,7 +8,10 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
@@ -34,7 +37,9 @@ fun Home(viewModel: HomeViewModel, navController: NavController) {
             null -> {}
         }
     }
-    val scrollBehavior = remember { TopAppBarDefaults.pinnedScrollBehavior() }
+    val scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior(
+        rememberTopAppBarScrollState()
+    )
     val items by viewModel.items.observeAsState(emptyMap())
     Scaffold(
         modifier = Modifier
